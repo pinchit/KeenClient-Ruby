39704cad5c96ba64e8dc6ab54ca3d906c1d95dc0
dorkitude <kyle@kylewild.com>
2012-06-19 10:28:02 -0700

    remove system_timer dependency

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -9,7 +9,7 @@ inst = Gem::DependencyInstaller.new
 
 begin
   if RUBY_VERSION < "1.9"
-    inst.install "system_timer", "=> 1.2.4"
+    #inst.install "system_timer", "=> 1.2.4"
   end
   rescue
     exit(1)
